95f846caad59827b47b9cd5010ba3c9e990571b6
gagdiez <gagdiez.c@gmail.com>
2024-05-21 17:48:56 -0300

    updatesg

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,27 +1,24 @@
-import styles from "@/styles/app.module.css";
-import { useEffect } from "react";
-import { useStore } from "@/layout";
-import Messages from "@/components/Messages";
-import { useState } from "react";
-import SignIn from "@/components/SignIn";
-import Form from "@/components/Form";
 import { utils } from "near-api-js";
+import { useState, useEffect, useContext } from "react";
+
+import Form from "@/components/Form";
+import SignIn from "@/components/SignIn";
+import Messages from "@/components/Messages";
+import styles from "@/styles/app.module.css";
+
+import { NearContext } from "@/context";
 import { GuestbookNearContract } from "@/config";
 
 export default function Home() {
-	const { signedAccountId, wallet } = useStore();
+	const { signedAccountId, wallet } = useContext(NearContext);
 	const [messages, setMessages] = useState([]);
 
 	useEffect(() => {
-		const initFunction = async () => {
-			const messages = await getLast10Messages();
-			setMessages(messages.reverse());
-		};
-		initFunction();
-	}, [wallet]);
+		getLast10Messages()
+			.then(messages => setMessages(messages.reverse()));
+	}, []);
 
 	const getLast10Messages = async () => {
-		if (!wallet) return [];
 		const total_messages = await wallet.viewMethod({
 			contractId: GuestbookNearContract,
 			method: "total_messages",
@@ -44,7 +41,7 @@ export default function Home() {
 		// Add message to the guest book
 		const deposit = utils.format.parseNearAmount(donation.value);
 		await wallet.callMethod({
-			contractId: CONTRACT_NAME,
+			contractId: GuestbookNearContract,
 			method: "add_message",
 			args: { text: message.value },
 			deposit,
@@ -60,17 +57,11 @@ export default function Home() {
 		message.focus();
 	};
 
-	const [loggedIn, setLoggedIn] = useState(false);
-
-	useEffect(() => {
-		setLoggedIn(!!signedAccountId);
-	}, [signedAccountId]);
-
 	return (
 		<main className={styles.main}>
 			<div className="container">
 				<h1>📖 NEAR Guest Book</h1>
-				{loggedIn ? (
+				{signedAccountId ? (
 					<Form onSubmit={onSubmit} currentAccountId={wallet.accountId} />
 				) : (
 					<SignIn />
