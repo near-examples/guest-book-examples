9b528c280f143f81c6b263470c9dc138006a809d
Den <mirloxd3@gmail.com>
2024-07-20 21:39:29 +0200

    feat(contract-ts): add borsh serialization after upgrading near-sdk-js to 2.0.0

--- contract-ts/src/contract.ts
+++ contract-ts/src/contract.ts
@@ -1,9 +1,17 @@
-import { NearBindgen, near, call, view, Vector } from 'near-sdk-js'
-import { POINT_ONE, PostedMessage } from './model'
+import { NearBindgen, near, call, view } from 'near-sdk-js';
+import { MessageSchema, POINT_ONE, PostedMessage } from './model';
+import * as borsh from 'borsh';
 
-@NearBindgen({})
+@NearBindgen({
+  serializer(value) {
+    return borsh.serialize(schema, value);
+  },
+  deserializer(value) {
+    return borsh.deserialize(schema, value);
+  },
+})
 class GuestBook {
-  messages: Vector<PostedMessage> = new Vector<PostedMessage>("v-uid");
+  messages: Array<PostedMessage> = [];
 
   @call({ payableFunction: true })
   // Public - Adds a new message.
@@ -19,9 +27,19 @@ class GuestBook {
   @view({})
   // Returns an array of messages.
   get_messages({ from_index = 0, limit = 10 }: { from_index: number, limit: number }): PostedMessage[] {
-    return this.messages.toArray().slice(from_index, from_index + limit);
+    return this.messages.slice(from_index, from_index + limit);
   }
 
   @view({})
   total_messages(): number { return this.messages.length }
 }
+
+const schema: borsh.Schema = {
+  struct: {
+    messages: {
+      array: {
+        type: MessageSchema,
+      },
+    },
+  },
+};
