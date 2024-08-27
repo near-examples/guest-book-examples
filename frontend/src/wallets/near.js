28a02f8815a01ef2aa6537c10860d9c55a308573
Ruben Marcus <ruben@rubenmarcus.dev>
2024-08-27 18:30:05 +0100

    add bitte wallet to the example

--- frontend/src/wallets/near.js
+++ frontend/src/wallets/near.js
@@ -8,6 +8,7 @@ import { setupModal } from "@near-wallet-selector/modal-ui";
 import { setupWalletSelector } from "@near-wallet-selector/core";
 import { setupHereWallet } from "@near-wallet-selector/here-wallet";
 import { setupMyNearWallet } from "@near-wallet-selector/my-near-wallet";
+import { setupBitteWallet } from "@near-wallet-selector/bitte-wallet";
 
 const THIRTY_TGAS = "30000000000000";
 const NO_DEPOSIT = "0";
@@ -35,7 +36,7 @@ export class Wallet {
   startUp = async (accountChangeHook) => {
     this.selector = setupWalletSelector({
       network: this.networkId,
-      modules: [setupMyNearWallet(), setupHereWallet()],
+      modules: [setupMyNearWallet(), setupHereWallet(), setupBitteWallet()],
     });
 
     const walletSelector = await this.selector;
