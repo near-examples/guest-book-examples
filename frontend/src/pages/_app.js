3d0298f671cc6c010dadfcb012dc79dfcdb576da
Amira Nasri <nasriamira88@gmail.com>
2025-02-12 14:26:04 +0100

    add wallets

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -3,16 +3,36 @@ import '@/styles/globals.css';
 import '@near-wallet-selector/modal-ui/styles.css';
 import { setupMyNearWallet } from '@near-wallet-selector/my-near-wallet';
 import { setupMeteorWallet } from '@near-wallet-selector/meteor-wallet';
+import { setupMeteorWalletApp } from '@near-wallet-selector/meteor-wallet-app';
+import { setupBitteWallet } from '@near-wallet-selector/bitte-wallet';
+import { setupEthereumWallets } from '@near-wallet-selector/ethereum-wallets';
+import { setupHotWallet } from '@near-wallet-selector/hot-wallet';
+import { setupLedger } from '@near-wallet-selector/ledger';
+import { setupSender } from '@near-wallet-selector/sender';
+import { setupHereWallet } from '@near-wallet-selector/here-wallet';
+import { setupNearMobileWallet } from '@near-wallet-selector/near-mobile-wallet';
+import { setupWelldoneWallet } from '@near-wallet-selector/welldone-wallet';
 import { HelloNearContract, NetworkId } from '@/config';
 import { WalletSelectorProvider } from '@near-wallet-selector/react-hook';
 import { Navigation } from '@/components/Navigation';
+import { wagmiConfig, web3Modal } from '@/wallets/web3modal';
+
  
 const walletSelectorConfig = {
   network: NetworkId,
   createAccessKeyFor: HelloNearContract,
   modules: [
-    setupMyNearWallet(),
+    setupEthereumWallets({ wagmiConfig, web3Modal, alwaysOnboardDuringSignIn: true }),
+    setupBitteWallet(),
     setupMeteorWallet(),
+    setupMeteorWalletApp({contractId: HelloNearContract}),
+    setupHotWallet(),
+    setupLedger(),
+    setupSender(),
+    setupHereWallet(),
+    setupNearMobileWallet(),
+    setupWelldoneWallet(),
+    setupMyNearWallet(),
   ],
 }
 
